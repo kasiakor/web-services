374b7e263f05ad9378a7e8a81d6ee26949447309
kasia.kordys <kasia.kordys@gmail.com>
2020-12-14 21:25:47 +0000

    initial commit

---
+++ WebServices/Models/ReservationRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServices.Models
+{
+    public class ReservationRepository
+    {
+        private static ReservationRepository repo = new ReservationRepository();
+
+        //current property, RR instance, will be consistent between requests
+        public static ReservationRepository Current
+        {
+            get
+            {
+                return repo;
+            }
+        }
+
+       
+    }
+}
