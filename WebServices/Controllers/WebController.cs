7a25f96a5b06d7d8d9feb61e6db4a805950d5ca7
kasia.kordys <kasia.kordys@gmail.com>
2020-12-15 20:22:46 +0000

    add web controller

---
+++ WebServices/Controllers/WebController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+
+        private ReservationRepository repo = ReservationRepository.Current;
+        // GET: Web
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservations( int id)
+        {
+            return repo.Get(id);
+        }
+
+        public Reservation PostReservations(Reservation item)
+        {
+            return repo.Add(item);
+        }
+
+        public void  DeleteReservations(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
