603b3cf5e5e38b312b106ae978cbd07cb52c5616
kasia.kordys <kasia.kordys@gmail.com>
2020-12-15 14:30:17 +0000

    add reservation repository

--- WebServices/Models/ReservationRepository.cs
+++ WebServices/Models/ReservationRepository.cs
@@ -17,7 +17,64 @@ namespace WebServices.Models
                 return repo;
             }
         }
+        //create in-memory reservation collection
+        private List<Reservation> data = new List<Reservation>
+        {
+            new Reservation
+            {
+                ReservationID = 1, ClientName = "Gia", Location = "Board Room"
+            },
+             new Reservation
+            {
+                ReservationID = 2, ClientName = "Celina", Location = "Rest Room"
+            },
+              new Reservation
+            {
+                ReservationID = 3, ClientName = "Cherry", Location = "Meeting Room"
+            }
+        };
 
-       
+        //define CRUD methods
+
+        public IEnumerable<Reservation> GetAll()
+        {
+            return data;
+        }
+
+        public Reservation Get(int id)
+        {
+            return data.Where(r => r.ReservationID == id).FirstOrDefault();
+        }
+
+        public Reservation Add(Reservation item)
+        {
+            item.ReservationID = data.Count + 1;
+            data.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            Reservation item = Get(id);
+            if(item != null)
+            {
+                data.Remove(item);
+            }
+        }
+
+        public bool Update(Reservation item)
+        {
+            Reservation storedItem = Get(item.ReservationID);
+            if (storedItem != null)
+            {
+                storedItem.ClientName = item.ClientName;
+                storedItem.Location = item.Location;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
