ea16d5d341f73613d06a35b06785f699236664b6
kasia.kordys <kasia.kordys@gmail.com>
2020-12-17 22:23:58 +0000

    simplify home controller

--- WebServices/Controllers/HomeController.cs
+++ WebServices/Controllers/HomeController.cs
@@ -9,43 +9,9 @@ namespace WebServices.Controllers
 {
     public class HomeController : Controller
     {
-
-        private ReservationRepository repo = ReservationRepository.Current;
-        // GET: Home
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            return View(repo.GetAll());
-        }
-
-        public ActionResult Add(Reservation item)
-        {
-            if(ModelState.IsValid)
-            {
-                repo.Add(item);
-                return RedirectToAction("Index");
-            }
-           else
-            {
-                return View("Index");
-            }
-        }
-
-        public ActionResult Remove(int id)
-        {
-            repo.Remove(id);
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Update(Reservation item) 
-        {
-            if(ModelState.IsValid && repo.Update(item))
-            {
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View("Index");
-            }
+            return View();
         }
     }
 }
